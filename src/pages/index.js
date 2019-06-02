b06d794a86daae6040de7ae90b21a8e80e05319e
Trapti Rahangdale <imtrapti1410@gmail.com>
2019-06-03 01:43:16 +0530

    basic setup for writing section

--- src/pages/index.js
+++ src/pages/index.js
@@ -29,12 +29,18 @@ const Home = () => {
 						<img src={profile} alt="Trapti Rahangdale" />
 					</figure>
 					<p className="content">
-						Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+						Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. 
+					</p>
+					<p className="content">
+					  Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+					</p>
+					<p className="content">
+					  when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
 					</p>
 				</section>
-				<section>
-					<h2>Writing</h2>
-					<ul>
+				<section className="home-writing-container">
+					<h2 className="section-title">Writing</h2>
+					<ul className="blogs-list">
 						{blogs.map((blog, index) => <li key={index}>
 							<h3>{blog}</h3>
 						</li>)}
