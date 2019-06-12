1a3dff9a40e6380e881a8275451d6013281e7f91
Trapti Rahangdale <imtrapti1410@gmail.com>
2019-06-12 23:47:38 +0530

    blog ui

--- src/pages/layout-components/header.js
+++ src/pages/layout-components/header.js
@@ -1,20 +1,44 @@
-import React from "react";
+import React from "react"
 
 const Header = () => {
   return (
     <header className="header">
       <h1 className="logo">
-        <svg xmlns="http://www.w3.org/2000/svg" width="300px" height="100px" viewBox="0 0 300 100" stroke="currentColor">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width="300px"
+          height="100px"
+          viewBox="0 0 300 100"
+          stroke="currentColor"
+        >
           <title id="logo">logo</title>
           <defs>
-            <path id="curvePath" d="M 0 60 C 140 90 190 0 290 50 " stroke="#D17702" fill="none" />
+            <path
+              id="curve-path-1"
+              d="M 0 60 C 140 90 190 0 290 50 "
+              stroke-width="3"
+              stroke="#f5b326"
+              fill="none"
+            />
+            <path
+              id="curve-path-2"
+              d="M 0 56 C 140 86 190 0 290 46 "
+              stroke-width="3"
+              stroke="#b4e6ff"
+              fill="none"
+            />
           </defs>
-          <use xlinkHref="#curvePath" />
+          <use xlinkHref="#curve-path-1" />
+          <use xlinkHref="#curve-path-2" />
           <text fontSize="3rem" x="0" y="0" className="name-text">
-            <textPath xlinkHref="#curvePath" >
-              <tspan fontSize="7rem" dy="25">T</tspan>
+            <textPath xlinkHref="#curve-path-1">
+              <tspan fontSize="7rem" dy="25">
+                T
+              </tspan>
               <tspan dx="-15">rapti</tspan>
-              <tspan fontSize="7rem" dx="10" dy="2" rotate="10">R</tspan>
+              <tspan fontSize="7rem" dx="10" dy="2" rotate="10">
+                R
+              </tspan>
               <tspan dx="-5">ahangdale</tspan>
             </textPath>
           </text>
@@ -25,4 +49,4 @@ const Header = () => {
   )
 }
 
-export default Header;
+export default Header
