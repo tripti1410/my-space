af58a96a0fe06e40a92cf8222da72769a3ceaa32
Trapti Rahangdale <imtrapti1410@gmail.com>
2020-07-04 16:41:39 +0530

    added 1st testimonial

---
+++ src/pages/home/testimonial-section/testimonial.js
@@ -0,0 +1,22 @@
+import React from "react"
+import "./testimonial.css"
+
+const Testimonial = () => {
+  return (
+    <div className="testimonial-section">
+      <h2 className="testimonial-title">What people say</h2>
+      <blockquote>
+        I had the chance to work with Trapti on a tricky svg animation, not only
+        she delivered a perfect work in time and struggled to solve challenging
+        points of the project, but she also gave me advices and followed up the
+        project in the next days, to check if everything was alright. I plan to
+        work with her in the future, and I would recommend her to anyone, not
+        only for her design and coding skills, but also for her super
+        professional attitude.
+        <footer>Alexandra Spalato - Co-Founder GatsbyWPThemes</footer>
+      </blockquote>
+    </div>
+  )
+}
+
+export default Testimonial
--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import AboutSection from "./home/about-section/about-section"
 import WritingSection from "./home/writing-section/writing-section"
 import PopularWorkSection from "./home/popular-work-section/popular-work-section"
 import Layout from "../layout-components/layout"
+import Testimonial from "./home/testimonial-section/testimonial"
 
 const Home = () => {
   return (
@@ -11,6 +12,7 @@ const Home = () => {
         <AboutSection />
         <WritingSection />
         <PopularWorkSection />
+        <Testimonial />
       </main>
     </Layout>
   )
