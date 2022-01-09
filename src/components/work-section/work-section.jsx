2aa133c65af80c1cfcbe5f003648263b8f511b37
Trapti Rahangdale <imtrapti1410@gmail.com>
2022-01-09 12:41:25 +0530

    lifesci project addeded

--- src/components/work-section/work-section.jsx
+++ src/components/work-section/work-section.jsx
@@ -6,10 +6,13 @@ const WorkSection = () => {
     <section className="home-work-container">
       <h2 className="section-title">Work</h2>
       <div className="work">
-        <div className="work-preview">
+        <figure className="work-preview">
         <img src="/assets/LSC-home-page-full.jpg" alt="homepage design of lifesci communications" />
-        </div>
-        <p>This is the website I created for Lifesci communications. Read details about this project <a href="/project/lifesci-project-details">here</a></p>
+        </figure>
+        <figcaption>
+        <a href="/project/lifesci-project-details">Lifesci Communications </a>
+        </figcaption>
+
       </div>
 
     </section>
