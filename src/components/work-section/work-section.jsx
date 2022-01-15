e0f507a5e0099d65b4b6bc3d6bcb5ddb2d93db3b
Trapti Rahangdale <imtrapti1410@gmail.com>
2022-01-15 09:28:51 +0530

    added space tourism

--- src/components/work-section/work-section.jsx
+++ src/components/work-section/work-section.jsx
@@ -12,9 +12,18 @@ const WorkSection = () => {
         <figcaption>
         <a href="/project/lifesci-project-details">LifeSci Communications </a>
         </figcaption>
-
       </div>
-
+      <div>
+      <figure >
+        <img src="/assets/space-tourism-home-page.png" alt="homepage design of Space Tourism" />
+      </figure>
+      <figcaption>
+        <a href="https://space-tourism-trapti.netlify.app" target="_blank">Space tourism</a>
+      </figcaption>
+      <p>
+        I wanted to get familiar with <a href="https://astro.build">Astro</a>. So I used <a href="https://www.frontendmentor.io/challenges">Frontend Mentor's</a> challenge design and created this in Plain HTML, CSS and Javascript.
+       </p>
+      </div>
     </section>
   )
 }
