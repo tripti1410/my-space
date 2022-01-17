6ba4d2cdfb247720f1427fca2325a4e2411d7790
Amith George <strider.am@gmail.com>
2022-01-17 19:27:00 +0530

    fix className issue in jsx

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 const Footer = () => {
   return (
     <footer className="footer">
-      <a class="footer-link" href="/rss.xml">Articles RSS Feed</a>
+      <a className="footer-link" href="/rss.xml">Articles RSS Feed</a>
       <p className="copyright-text"><strong>© 2019 - Today / Copyright Trapti Rahangdale. All rights reserved.</strong></p>
       <p className="site-info">
         This site is built with <a href="https://astro.build">Astro</a> and hosted on <a href="https://netlify.com">Netlify</a>. The source code is hosted on <a href="https://github.com">Github</a>.
