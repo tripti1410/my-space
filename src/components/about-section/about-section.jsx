12904b395a8831ded80ab0e56c1ce5f70fa6c48b
Trapti Rahangdale <imtrapti1410@gmail.com>
2022-08-10 09:12:45 +0530

    changed profile image

--- src/components/about-section/about-section.jsx
+++ src/components/about-section/about-section.jsx
@@ -7,35 +7,40 @@ const AboutSection = ({ profileImg }) => {
       <figure className="image-container">
         <img src={profileImg} alt="Trapti rahangdale" />
       </figure>
-      <h1 className="about-heading">
-        Hi, I'm Trapti. I{" "}
-        <svg
-          className="heart"
-          width="40px"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 25.1 25"
-        >
-          <path d="M25,8.8C25.2,2,16.8-1.9,12.2,3.1h0c-7-6-16.1,4.5-10.6,11.3h0l.6.7,10,10.7L22.9,14.4h-.1A7.9,7.9,0,0,0,25,8.8Z" />
-        </svg>{" "}
-        SVG, animations and Web
-      </h1>
-      <p className="content">
-        I am a freelance Creative frontend engineer based in India. I can help you with creating and
-        optimizing SVGs, creating web animations, responsive layouts, websites,
-        webapps and many more.
-      </p>
-      <p className="content">
-        I emphasize on <strong>accessibility</strong> and{" "}
-        <strong>performance</strong> in my work.
-      </p>
-      <p className="content">
-        I blog on these topics and I am available as a guest author.
-      </p>
-      <p className="content">
-        <a href="/about" className="link">
-          Know more
-        </a>
-      </p>
+      <div class="home-intro">
+        <h1 className="about-heading">
+          Hi, I'm Trapti. I{" "}
+          <svg
+            className="heart"
+            width="40px"
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 25.1 25"
+          >
+            <path d="M25,8.8C25.2,2,16.8-1.9,12.2,3.1h0c-7-6-16.1,4.5-10.6,11.3h0l.6.7,10,10.7L22.9,14.4h-.1A7.9,7.9,0,0,0,25,8.8Z" />
+          </svg>{" "}
+          SVG, animations and Web
+        </h1>
+        <p className="content">
+          I am a freelance Creative frontend engineer based in India. I can help
+          you with creating and optimizing SVGs, creating web animations,
+          responsive layouts, websites, webapps and many more.
+        </p>
+        <p className="content">
+          I emphasize on <strong>accessibility</strong> and{" "}
+          <strong>performance</strong> in my work.
+        </p>
+        <p className="content">
+          I blog on these topics and I am available as a guest author.
+        </p>
+        <p className="content">
+          Learn more &nbsp;
+          <a href="/about" className="link">
+            about me.
+          </a>
+          {/* or
+        <a href="mailto:hello@trapti.dev">hire me.</a> */}
+        </p>
+      </div>
     </section>
   )
 }
