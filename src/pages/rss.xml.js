f5508f17b98923dced0b55657a717ad8bb06cdfa
Trapti Rahangdale <imtrapti1410@gmail.com>
2022-08-28 16:16:06 +0530

    use rss integration to generate rss feed

---
+++ src/pages/rss.xml.js
@@ -0,0 +1,22 @@
+import rss from "@astrojs/rss"
+import * as blog from "$src/js/posts.js"
+
+const allPosts = Object.values(
+  import.meta.glob("../content/posts/*.md", { eager: true })
+)
+const posts = blog.sortedPosts(allPosts)
+
+export const get = () =>
+  rss({
+    title: "Trapti Rahangdale's blog",
+    site: import.meta.env.SITE,
+    description: "My experiences",
+    items: posts.map((item) => ({
+      title: item.frontmatter.title,
+      description: item.description,
+      link: item.frontmatter.path,
+      pubDate: item.frontmatter.date,
+    })),
+    // (optional) inject custom xml
+    customData: `<language>en-us</language>`,
+  })
