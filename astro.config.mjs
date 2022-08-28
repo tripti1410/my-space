57189606cb444df27662497aa4b5ad51f92f34bf
Trapti Rahangdale <imtrapti1410@gmail.com>
2022-08-28 16:59:07 +0530

    removed comments

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,7 @@ import { defineConfig } from "astro/config"
 import react from "@astrojs/react"
 import sitemap from "@astrojs/sitemap"
 
-// https://astro.build/config
 export default defineConfig({
-  // your configuration options here...
   integrations: [react(), sitemap()],
   site: "https://www.iamtrapti.com/",
 })
